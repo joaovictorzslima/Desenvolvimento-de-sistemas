c376a78bb7bed6b47651634faae33cda42d30f8f
joaovictorzslima <joaovictoo2@outlook.com>
2025-04-04 19:50:30 -0300

    Create inde.php

---
+++ inde.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 05 - HOME</title>
+</head>
+<body>
+    <h1>Aula 05 - HOME</h1>
+
+    <h2>Cadastro Folha de Pagamentos</h2>
+
+    <form action="folha.php" method="post">
+        <p>
+        <input type="text" name="funcionario" 
+        placeholder="Nome do funcionario" required>
+        </p>
+        
+        <p>
+        <input type="numer" name="horas_trab" 
+        placeholder="Horas trabalhadas" required>
+        </p>
+        
+        <p>    
+        <input type="number" name="valor_hora" 
+        placeholder="Valor da hora R$" required>
+        </p>
+
+        <p>
+            <button type="submit">Calcular</button>
+        </p>
+    </form>
+</body>
+</html>
